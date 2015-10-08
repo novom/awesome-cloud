ff408eb880926e4f1cb2855b2f95ec2ec2417607
Steve <stiv.aw@gmail.com>
2015-10-08 15:17:54 -0400

    add git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'This is a wrapper for the http://www.jqueryscript.net/text/Awesome-Tag-Cloud-Plugin-with-jQuery-Html5-Canvas-awesomeCloud.html',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/novom/awesome-cloud.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
